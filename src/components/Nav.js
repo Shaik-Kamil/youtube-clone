84651d91bb3f4725d2b247a9607f1f72ef89f298
TrianePeart <TrianePeart@gmail.com>
2022-11-15 10:44:16 -0500

    Syntax error corrected in Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Nav.css'
 import {Link} from 'react-router-dom'
 
-export default function Nav (props) {
+export default function Nav () {
     return (
         <nav className='NavBar'>
             <Link>
