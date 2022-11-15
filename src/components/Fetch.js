f59699b4ed9b638e3f0a224fcede4961c3240731
TrianePeart <TrianePeart@gmail.com>
2022-11-15 11:08:41 -0500

    Created an Error Function

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,1 +1,11 @@
 //Export functions from here. 
+
+export default function ErrorMessage() {
+    return (
+      <section role="error message" className="error">
+        <p>Something went wrong!</p>
+      </section>
+    );
+  }
+
+//Error Message Created. Edit To Be Modal. 
