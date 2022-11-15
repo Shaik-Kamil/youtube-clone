ea5f8e3d6432c5c950a8be72a199897ea4f1af81
TrianePeart <TrianePeart@gmail.com>
2022-11-15 09:23:27 -0500

    Created Functional Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './Nav.css'
+import {Link} from 'react-router-dom'
 
 export default function Nav (props) {
     return (
-        <div>
-            Nav
-        </div>
+        <nav className='NavBar'>
+            <Link>
+            <img src='https://www.freeiconspng.com/thumbs/youtube-logo-png/youtube-logo-drawn-7.png'/>
+            </Link>
+            <Link exact to='/'>
+                Home
+            </Link>
+            <Link to='/About'>
+                About
+            </Link>
+
+        </nav>
     );
 }
 
