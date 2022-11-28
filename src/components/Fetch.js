12eaf8a939725e354f4474029ddd1d883eec4c33
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-11-28 14:29:53 -0500

    fixed comments mapping and video title

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -4,12 +4,12 @@ let key = process.env.REACT_APP_API_KEY;
 
 export function showApi(id) {
   return fetch(
-    `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=${key} `
+    `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=${key}`
   ).then((res) => res.json());
 }
 
 export function searchVideo(searchInput) {
   return fetch(
     `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchInput}&key=${key}&maxResults=20`
-  ).then((res) => res.json())
+  ).then((res) => res.json());
 }
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,26 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import YouTube from 'react-youtube';
 import { useParams } from 'react-router-dom';
-import './Video.css'
+import './Video.css';
 import { showApi } from './Fetch';
-export default function Video({videos}) {
+export default function Video({ videos }) {
   const { id } = useParams();
-  const [like, setLike] = useState(0)
-  const [dislike, setDislike] = useState(0)
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0);
   const [userComment, setUserComment] = useState({
-    comment:'',
-    user:'',
-  })
-  const [comments] = useState([])
-  const [show, setShow] = useState([])
-  
+    comment: '',
+    user: '',
+  });
+  const [comments] = useState([]);
+  const [show, setShow] = useState([]);
+
   useEffect(() => {
-    showApi()
+    showApi(id)
       .then((res) => {
         setShow(res.items);
       })
       .catch((err) => {
-
+        console.log(err);
       });
   }, []);
 
@@ -36,54 +36,95 @@ export default function Video({videos}) {
     allow: 'autoplay',
     allowFullScreen: true,
   };
-console.log(show)
-  // function handleComment
+
+  console.log(show);
   return (
-    <div className='picture'>
+    <div className="picture">
       <section>
-      <div>
-      <YouTube videoId={id} opts={opts} />
-      <section>
-      {show.map((video) => {
-        return (
-          <div>
-              <p>{video.snippet.title}</p>
+        <div>
+          <YouTube videoId={id} opts={opts} />
+          <section>
+            {show.map((video) => {
+              return (
+                <div>
+                  <h1>{video.snippet.title}</h1>
+                  <h2>{video.snippet.channelTitle}</h2>
+                </div>
+              );
+            })}
+          </section>
+          <br />
+          <hr />
+          <div className="votes">
+            <button
+              className="likes"
+              onClick={() => {
+                setLike(like + 1);
+              }}
+            >
+              {' '}
+              ⬆Like {like}
+            </button>
+            &nbsp;
+            <button
+              className="dislikes"
+              onClick={() => {
+                setDislike(dislike + 1);
+              }}
+            >
+              {' '}
+              ⬇ Dislike{' '}
+            </button>
           </div>
-        );
-      })}
-    
-        {/* Does not work with T or accessing with ID. Tried using its own data param but might be understanding it wrong */}
+        </div>
+        <form
+          className="commentForm"
+          onSubmit={(e) => {
+            e.preventDefault();
+            comments.push(userComment);
+            setUserComment({
+              comment: '',
+              user: '',
+            });
+          }}
+        >
+          <label htmlFor="name">User Name</label>
+          &nbsp;
+          <input
+            id="user"
+            type="text"
+            placeholder="Enter Name Here"
+            onChange={(e) =>
+              setUserComment({ ...userComment, [e.target.id]: e.target.value })
+            }
+          />
+          <br />
+          <br />
+          <label htmlFor="comment">Comment</label>
+          &nbsp;
+          <input
+            id="comment"
+            type="text"
+            placeholder="Enter Comment Here"
+            onChange={(e) =>
+              setUserComment({ ...userComment, [e.target.id]: e.target.value })
+            }
+          />
+          &nbsp;
+          <button type="submit">Submit</button>
+        </form>
       </section>
-      <br/>
-      <hr/>
-      <div className='votes'>
-      <button className='likes' onClick={()=>{setLike(like + 1)}}> ⬆Like {like}</button>
-      &nbsp;
-      <button className='dislikes' onClick={()=>{setDislike(dislike + 1)}}> ⬇ Dislike </button>
+      <div>
+        <ul>
+          {comments.map((el) => {
+            return (
+              <li>
+                {el.user}: {el.comment}
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    </div>
-    <form className='commentForm' onSubmit={(e)=>{
-    e.preventDefault();
-    setUserComment({
-      comment:'',
-      user:'',
-    });
-    }}>
-      <label htmlFor='name'>User Name</label>
-      &nbsp;
-      <input id='user' type='text' placeholder='Enter Name Here' onChange={(e) => setUserComment({...userComment, [e.target.id]: e.target.value})}/>
-      <br/>
-      <br/>
-      <label htmlFor='comment'>Comment</label>
-      &nbsp;
-      <input id='comment' type='text' placeholder='Enter Comment Here'  onChange={(e) => setUserComment({...userComment, [e.target.id]: e.target.value})} />
-      &nbsp;
-      <button type='submit' >Submit</button>
-      </form>
-      {/* <ul>
-    
-      </ul> */}
-      </section>
     </div>
   );
 }
